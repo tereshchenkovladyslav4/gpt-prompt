0bbdf394c8344a5395357745e9295fb093f191b6
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2023-03-23 16:20:10 +0200

    fix icons

--- frontend/src/app/components/layout/layout.component.ts
+++ frontend/src/app/components/layout/layout.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { AuthService, MediaService } from '@services';
 import { UserInfo } from '@models';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { environment } from '@env/environment';
+import { DOCUMENT } from '@angular/common';
+
 @UntilDestroy()
 @Component({
   selector: 'app-layout',
@@ -15,7 +17,11 @@ export class LayoutComponent implements OnInit {
   public user: UserInfo | undefined;
   public menuHidden = false;
 
-  constructor(public mediaService: MediaService, public authService: AuthService) {}
+  constructor(
+    public mediaService: MediaService,
+    public authService: AuthService,
+    @Inject(DOCUMENT) private document: Document,
+  ) {}
 
   ngOnInit() {
     this.authService
@@ -32,8 +38,23 @@ export class LayoutComponent implements OnInit {
     this.menuHidden = !this.menuHidden;
   }
 
-  public toggleSidenav() {
+  toggleSidenav() {
     this.sidenav.toggle();
+  }
+
+  closeSubMenus() {
+    let menu = this.document.querySelector('.sidenav-menu-outer');
+    if (menu) {
+      for (let i = 0; i < menu.children[0].children.length; i++) {
+        let child = menu.children[0].children[i];
+        if (child) {
+          if (child.children[0].classList.contains('expanded')) {
+            child.children[0].classList.remove('expanded');
+            child.children[1].classList.remove('show');
+          }
+        }
+      }
+    }
   }
 
   logout() {
